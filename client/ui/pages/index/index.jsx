1ae2b3241db62a6badec77f2b7bd7dc8c47436f8
Vanielle Le <vanielle@arch-iot.com>
2018-05-15 19:00:20 -0700

    first draft of emailing list

--- client/ui/pages/index/index.jsx
+++ client/ui/pages/index/index.jsx
@@ -39,31 +39,44 @@ export default class IndexPage extends Component {
   render() {
     return (
       <div>
+
+
         <FeaturedWork title="Passion Projects" renderPictureBlock={this.renderProjects()} />
 
-        <SkillProgress renderLanguages={this.renderLanguages()} renderTechnologies={this.renderTechnologies()} />
-
-        {/* <div className="container">
-
-        <div className="page-header">
-          <h1>Example page header <small>Subtext for header</small>
-
-          <div className="input-group">
-
-            <input type="text" className="form-control" aria-label="..." />
+        <div className="page-header text-center" style={{
+          'border-top': '10px solid #000',
+          'border-left': '30px solid #000',
+          'border-right': '30px solid #000',
+          'border-bottom': '1px solid #000',
+          'padding': '2em',
+          'margin': 0
+        }}>
+          <h3>Enter your email below and I'll send you a <i>free</i> resume template. <small>No spam. Ever.</small>
+          <div className="input-group"
+               style={{
+                 'padding': '1em 8em 0 8em'
+               }}>
+            <input type="text"
+                   className="form-control"
+                   aria-label="..."
+                   placeholder="Enter email here..."/>
             <div className="input-group-btn">
-              <button className="btn btn-default">
-                <span className="glyphicon glyphicon-ok"></span>
-
+              <button className="btn btn-default"
+                      style={{
+                        'border-radius': '50%',
+                        'width': '40px',
+                        'height': '40px',
+                        'margin': '0px 0px 0px 10px'
+                      }}>
+                <span className="glyphicon glyphicon-send"></span>
               </button>
             </div>
           </div>
-
-          </h1>
-
+          </h3>
         </div>
 
-        </div> */}
+
+        <SkillProgress renderLanguages={this.renderLanguages()} renderTechnologies={this.renderTechnologies()} />
       </div>
     )
   }
