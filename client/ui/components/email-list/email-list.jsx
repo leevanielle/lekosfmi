6faaf16a6a63aa71b80e44ecc12781d9591441af
Vanielle Le <lephanvanielle@gmail.com>
2018-06-07 08:48:59 -0700

    feedback when email submits

--- client/ui/components/email-list/email-list.jsx
+++ client/ui/components/email-list/email-list.jsx
@@ -8,7 +8,7 @@ export default class EmailList extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: "Let's chat!" // default message
+      message: ""
     }
   }
 
@@ -18,17 +18,25 @@ export default class EmailList extends Component {
 
     let email = e.target.email.value
 
-    if (email) {
-      Meteor.call('email-list.insert', email, (error, result) => {
-
-        console.log('Result', error)
-        console.log('Error', result)
-
-      })
-
+    if (!email) {
+      return
     }
 
+    Meteor.call('email-list.insert', email, (error, result) => {
+      this.setState({
+        message: "Thanks you, should be receiving your resume template shortly!"
+      })
+    })
+
     e.target.email.value = ''
+  }
+
+  resetMessage() {
+    if (this.state.message === 0) {
+      return
+    }
+
+    this.setState({ message: '' })
   }
 
   render() {
@@ -40,12 +48,14 @@ export default class EmailList extends Component {
             Enter your email below and I'll send you a <i>free</i> resume template.
           </h3>
           <small>No spam. Ever.</small>
+          <p>{this.state.message ? this.state.message : null}</p>
           <div className="input-group">
             <input type="email"
                    name="email"
                    className="form-control"
                    aria-label="..."
-                   placeholder="Enter email here..."/>
+                   placeholder="Enter email here..."
+                   onChange={this.resetMessage.bind(this)} />
             <div className="input-group-btn">
               <button className="btn btn-default"
                       type="submit">
